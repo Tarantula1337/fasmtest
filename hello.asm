67c6abb58f0945d81ac8453a1e9eefd2a1dd8fad
Tarantula1337 <tarantulalp@gmail.com>
2023-12-14 00:17:05 +0100

    added macros

--- hello.asm
+++ hello.asm
@@ -1,17 +1,27 @@
 format ELF64 executable
 
+macro exit code
+{
+    mov rax, 60
+    mov rdi, code
+    syscall
+}
+
+macro write fd, buf, buflen
+{
+    mov rax, 1
+    mov rdi, fd
+    mov rsi, buf
+    mov rdx, buflen
+    syscall
+}
+
 segment readable executable
 entry main
 main:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, msg_len
-    syscall
-    
-    mov rax, 60
-    mov rdi, 0
-    syscall
+
+    write 1, msg, msg_len
+    exit 0
 
 segment readable writeable
 msg db "Hello, World!", 10
